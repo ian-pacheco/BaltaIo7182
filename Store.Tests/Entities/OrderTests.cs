670105b15cc5ab35df379a9a988684a3cbde6ec8
Ian Pacheco <ianpacheco64@gmail.com>
2019-08-17 02:06:26 -0300

    Iniciando testes, XUnit e Flunt

---
+++ Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,84 @@
+﻿using Store.Domain.Entities;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Store.Tests.Entities
+{
+    public class OrderTests
+    {
+        private Customer CUSTOMER = new Customer("Ian Pacheco", "ianpacheco@me.com");
+        private Order ORDER => new Order(CUSTOMER, 0, null);
+
+        [Fact]
+        public void ShouldBeGenerateNewValidOrder8CharNumber()
+        {
+            Assert.Equal(8, ORDER.Number.Length);
+        }
+
+        [Fact]
+        public void ShouldBeStatusWaitingPaymentWhenGivenNewOrder()
+        {
+            Assert.True(false, "message");
+        }
+
+        [Fact]
+        public void ShouldBeStatusWaitingDeliveryWhenOrderPaid()
+        {
+            Assert.True(false, "message");
+        }
+
+        [Fact]        
+        public void ShouldBeStatusCanceledWhenStatusCanceled()
+        {
+            Assert.True(false, "message");
+        }
+
+        [Fact]        
+        public void ShouldNotBeAddNewItemWithoutProductAdded()
+        {
+            Assert.True(false, "message");
+        }
+
+        [Fact]        
+        public void ShouldNotAddItemWithoutQuantityLikeZeroOrBelow()
+        {
+            Assert.True(false, "message");
+        }
+
+        [Fact]        
+        public void ShouldBeTotalLike50WhenNewValidOrder()
+        {
+            Assert.True(false, "message");
+        }
+
+        [Fact]        
+        public void ShouldBeTotaLike60WhenGivenExpiredDiscount()
+        {
+            Assert.True(false, "message");
+        }
+
+        [Fact]        
+        public void ShouldBeTotalLike60WhenGivenAnInvalidDiscount()
+        {
+            Assert.True(false, "message");
+        }
+
+        [Fact]        
+        public void ShouldBeTotalLike50WhenDiscountLike10()
+        {
+            Assert.True(false, "message");
+        }
+
+        [Fact]        
+        public void ShouldBeTotalLike60WhenDeliveryFeeBeLike10()
+        {
+            Assert.True(false, "message");
+        }
+
+        [Fact]        
+        public void ShouldBeInvalidWhenOrderWithoutCustomer()
+        {
+            Assert.True(false, "message");
+        }
+    }
+}
