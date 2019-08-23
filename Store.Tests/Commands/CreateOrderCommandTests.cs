122cb65c24ad8a71a7c3b5eb665341828b0c9af1
Ian Pacheco <ianpacheco64@gmail.com>
2019-08-22 23:22:44 -0300

    Criação dos Commands e tests

---
+++ Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -0,0 +1,23 @@
+﻿using Store.Domain.Commands;
+using System;
+using Xunit;
+
+namespace Store.Tests.Commands
+{
+    public class CreateOrderCommandTests
+    {
+        [Fact]
+        public void ShouldNotBeCreatedOrderWithInvalidCommand()
+        {
+            var command = new CreateOrderCommand();
+            command.Customer = "";
+            command.ZipCode = "12345678";
+            command.PromoCode = "123456789";
+            command.Itens.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Itens.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Validate();
+
+            Assert.False(command.Valid);
+        }
+    }
+}
---
+++ Store/Commands/CreateOrderCommand.cs
@@ -0,0 +1,39 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Store.Domain.Commands.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Store.Domain.Commands
+{
+    public class CreateOrderCommand : Notifiable, ICommand
+    {
+        public CreateOrderCommand()
+        {
+            Itens = new List<CreateOrderItemCommand>();
+        }
+
+        public CreateOrderCommand(string customer, string zipCode, string promoCode, IList<CreateOrderItemCommand> itens)
+        {
+            Customer = customer;
+            ZipCode = zipCode;
+            PromoCode = promoCode;
+            Itens = itens;
+        }
+
+        public string Customer { get; set; }
+        public string ZipCode { get; set; }
+        public string PromoCode { get; set; }
+        public IList<CreateOrderItemCommand> Itens { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                .Requires()
+                .HasLen(Customer, 11, "Customer", "Cliente inválido")
+                .HasLen(ZipCode, 8, "ZipCode", "CEP inválido")
+            );
+        }
+    }
+}
